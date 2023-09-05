683278b8fecd5d5325ef866ef512a5d7d3b00a46
Pranav Teja ☑️ <86045290+Pranavtej@users.noreply.github.com>
2023-09-05 23:56:31 +0530

    test

--- server/src/old_inde.cjs
+++ server/src/old_inde.cjs
@@ -1,6 +1,17 @@
 
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use(cors());
 
+
+app.get('/', (req, res) => {
+    res.send('DevJobs API');
+
+  });
+
+  
 module.exports = (req, res) => {
-    res.status(200).json({ message: 'Hello, World!' });
+    app(req, res);
   };
   
